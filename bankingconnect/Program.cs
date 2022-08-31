e510c9c1970351104a3db4a9d94006440359c335
Mansi Saxena <Mansi.Saxena@bhavnacorp.com>
2022-08-31 10:17:53 +0530

    commit

---
+++ bankingconnect/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace bankingconnect
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
